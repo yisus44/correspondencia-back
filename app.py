df4ff24b56e670754a7dd37ac9c5a4e20415da66
yisus44 <jesusadrian1953.1@gmail.com>
2023-05-06 18:31:19 -0600

    refactor to use routes in a different file

--- app.py
+++ app.py
@@ -1,131 +1,24 @@
 
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
-from datetime import datetime
+
 load_dotenv()
-app = Flask(__name__)
-# Configure MariaDB connection
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DATABASE')}"
+db = SQLAlchemy()
 
- # Create SQLAlchemy object
-db = SQLAlchemy(app)
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] =f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DATABASE')}"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-from models import User  # import the User model
-with app.app_context():
-    db.create_all()
+    db.init_app(app)
 
-@app.route("/users",methods=["GET"])
-def fetch_users():
-     # Query the database
-    data = User.query.all()
+    with app.app_context():
+        db.create_all()
 
-    # Convert the data to a dictionary
-    data_dict = [row.__dict__ for row in data]
+    # Importing routes
+    from routes import user_routes
+    app.register_blueprint(user_routes)
 
-    # Remove the "_sa_instance_state" key from each dictionary
-    for row in data_dict:
-        row.pop('_sa_instance_state', None)
-
-    return str(data_dict)
-
-@app.route("/users", methods=["POST"])
-def create_user():
-    # Example POST data format: {"nombre": "John", "apellidoMaterno": "Doe", ...}
-    data = request.get_json()
-    fecha_nacimiento = datetime.strptime(data['fechaNacimiento'], '%Y-%m-%d %H:%M:%S')
-    new_user = User(
-        nombre=data["nombre"],
-        apellidoMaterno=data["apellidoMaterno"],
-        apellidoPaterno=data["apellidoPaterno"],
-        cargo=data["cargo"],
-        empresa=data["empresa"],
-        calle=data["calle"],
-        numeroExt=data["numeroExt"],
-        numeroInt=data["numeroInt"],
-        colonia=data["colonia"],
-        municipio=data["municipio"],
-        estado=data["estado"],
-        codigoPostal=data["codigoPostal"],
-        telefono=data["telefono"],
-        correoElectronico=data["correoElectronico"],
-        fechaNacimiento=fecha_nacimiento
-    )
-    db.session.add(new_user)
-    db.session.commit()
-    
-    return jsonify({"user": "User created successfully!"}), 201
-
-@app.route("/users/<int:user_id>", methods=["GET"])
-def get_user(user_id):
-    user = User.query.get(user_id)
-    if user:
-        user_dict = user.__dict__
-        user_dict.pop('_sa_instance_state', None)
-        return jsonify(user_dict)
-    else:
-        return jsonify({"message": "User not found."}), 404
-
-@app.route('/users/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    # Query the database for the user to update
-    user = User.query.get(user_id)
-
-    # Check if the user exists
-    if user is None:
-        return jsonify({'error': 'User not found'}), 404
-
-    # Update the user fields with the request body data
-    if 'nombre' in request.json:
-        user.nombre = request.json['nombre']
-    if 'apellidoMaterno' in request.json:
-        user.apellidoMaterno = request.json['apellidoMaterno']
-    if 'apellidoPaterno' in request.json:
-        user.apellidoPaterno = request.json['apellidoPaterno']
-    if 'cargo' in request.json:
-        user.cargo = request.json['cargo']
-    if 'empresa' in request.json:
-        user.empresa = request.json['empresa']
-    if 'calle' in request.json:
-        user.calle = request.json['calle']
-    if 'numeroExt' in request.json:
-        user.numeroExt = request.json['numeroExt']
-    if 'numeroInt' in request.json:
-        user.numeroInt = request.json['numeroInt']
-    if 'colonia' in request.json:
-        user.colonia = request.json['colonia']
-    if 'municipio' in request.json:
-        user.municipio = request.json['municipio']
-    if 'estado' in request.json:
-        user.estado = request.json['estado']
-    if 'codigoPostal' in request.json:
-        user.codigoPostal = request.json['codigoPostal']
-    if 'telefono' in request.json:
-        user.telefono = request.json['telefono']
-    if 'correoElectronico' in request.json:
-        user.correoElectronico = request.json['correoElectronico']
-    if 'fechaNacimiento' in request.json:
-        fecha_nacimiento = datetime.strptime(request.json['fechaNacimiento'], '%Y-%m-%d %H:%M:%S')
-        user.fechaNacimiento = fecha_nacimiento
-  
-    # Save the changes to the database
-    db.session.commit()
-    print("hola")
-    print("adios")
-    # Return the updated user as JSON
-
-    user_dict = user.__dict__
-    user_dict.pop('_sa_instance_state', None)
-    return jsonify(user_dict)
-
-
-@app.route("/users/<int:user_id>", methods=["DELETE"])
-def delete_user(user_id):
-    user = User.query.get(user_id)
-    if user:
-        db.session.delete(user)
-        db.session.commit()
-        return jsonify({"message": "User deleted successfully!"}), 200
-    else:
-        return jsonify({"message": "User not found."}), 404
+    return app
